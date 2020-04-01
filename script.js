dcca3c345b21ada560b55ccc52393dc6b29fdf38
ksabhi01 <ksabhishek639@gmail.com>
2020-04-01 18:53:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,258 @@
+var condition1=true;
+var condition2=true;
+var condition3=true;
+var condition4=true;
+var condition5=true;
+var condition6=true;
+// Head-section
+function head1move(){
+	var x= document.getElementById('head-1');
+		if(condition1)
+		{	
+			x.classList.add('head-1');
+			condition1=false;
+		}	
+		else
+		{	
+			x.classList.remove('head-1');
+			condition1=true;	
+		}	
+		
+}
+
+function head2move(){
+	var x= document.getElementById('head-2');
+		if(condition1)
+		{	
+			x.classList.add('head-2');
+			condition1=false;
+		}	
+		else
+		{	
+			x.classList.remove('head-2');
+			condition1=true;	
+		}	
+}
+
+function head3move(){
+	var x= document.getElementById('head-3');
+		if(condition1)
+		{	
+			x.classList.add('head-3');
+			condition1=false;
+		}	
+		else
+		{	
+			x.classList.remove('head-3');
+			condition1=true;	
+		}	
+}
+
+//Collar-section
+function collar1move(){
+	var x= document.getElementById('collar-1');
+		if(condition2)
+		{	
+			x.classList.add('collar-1');
+			condition2=false;
+		}	
+		else
+		{	
+			x.classList.remove('collar-1');
+			condition2=true;	
+		}	
+}
+
+function collar2move(){
+	var x= document.getElementById('collar-2');
+		if(condition2)
+		{	
+			x.classList.add('collar-2');
+			condition2=false;
+		}	
+		else
+		{	
+			x.classList.remove('collar-2');
+			condition2=true;	
+		}	
+}
+
+function collar3move(){
+	var x= document.getElementById('collar-3');
+		if(condition2)
+		{	
+			x.classList.add('collar-3');
+			condition2=false;
+		}	
+		else
+		{	
+			x.classList.remove('collar-3');
+			condition2=true;	
+		}	
+}
+
+//Eyes-Section
+
+function eye1move(){
+	var x= document.getElementById('eye-1');
+		if(condition3)
+		{	
+			x.classList.add('eye-1');
+			condition3=false;
+		}	
+		else
+		{	
+			x.classList.remove('eye-1');
+			condition3=true;	
+		}	
+}
+
+function eye2move(){
+	var x= document.getElementById('eye-2');
+		if(condition3)
+		{	
+			x.classList.add('eye-2');
+			condition3=false;
+		}	
+		else
+		{	
+			x.classList.remove('eye-2');
+			condition3=true;	
+		}	
+}
+
+function eye3move(){
+	var x= document.getElementById('eye-3');
+		if(condition3)
+		{	
+			x.classList.add('eye-3');
+			condition3=false;
+		}	
+		else
+		{	
+			x.classList.remove('eye-3');
+			condition3=true;	
+		}	
+}
+
+
+//Nose-Section
+
+function nose1move(){
+	var x= document.getElementById('nose-1');
+		if(condition4)
+		{	
+			x.classList.add('nose-1');
+			condition4=false;
+		}	
+		else
+		{	
+			x.classList.remove('nose-1');
+			condition4=true;	
+		}	
+}
+
+function nose2move(){
+	var x= document.getElementById('nose-2');
+		if(condition4)
+		{	
+			x.classList.add('nose-2');
+			condition4=false;
+		}	
+		else
+		{	
+			x.classList.remove('nose-2');
+			condition4=true;	
+		}	
+}
+
+function nose3move(){
+	var x= document.getElementById('nose-3');
+		if(condition4)
+		{	
+			x.classList.add('nose-3');
+			condition4=false;
+		}	
+		else
+		{	
+			x.classList.remove('nose-3');
+			condition4=true;	
+		}	
+}
+
+//Mouth-section
+
+function mouth1move(){
+	var x= document.getElementById('mouth-1');
+		if(condition5)
+		{	
+			x.classList.add('mouth-1');
+			condition5=false;
+		}	
+		else
+		{	
+			x.classList.remove('mouth-1');
+			condition5=true;	
+		}	
+}
+
+function mouth2move(){
+	var x= document.getElementById('mouth-2');
+		if(condition5)
+		{	
+			x.classList.add('mouth-2');
+			condition5=false;
+		}	
+		else
+		{	
+			x.classList.remove('mouth-2');
+			condition5=true;	
+		}	
+}
+
+function mouth3move(){
+	var x= document.getElementById('mouth-3');
+		if(condition5)
+		{	
+			x.classList.add('mouth-3');
+			condition5=false;
+		}	
+		else
+		{	
+			x.classList.remove('mouth-3');
+			condition5=true;	
+		}	
+
+}
+
+
+//Hand 
+
+
+function hand1move(){
+	var x= document.getElementById('hand-1');
+		if(condition6)
+		{	
+			x.classList.add('hand-1');
+			condition6=false;
+		}	
+		else
+		{	
+			x.classList.remove('hand-1');
+			condition6=true;	
+		}
+}	
+
+function hand2move(){
+	var x= document.getElementById('hand-2');
+		if(condition6)
+		{	
+			x.classList.add('hand-2');
+			condition6=false;
+		}	
+		else
+		{	
+			x.classList.remove('hand-2');
+			condition6=true;	
+		}
+}		
